4c0163f105f6c66676b04e6f49db7ed75a16894a
Robert Habermeier <rphmeier@gmail.com>
2016-04-16 04:09:19 -0400

    Add code for SDL2 integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include "SDL.h"
+#include "SDL_video.h"
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS) != 0) {
+        return 1;
+    }
+    
+    SDL_Window* window = SDL_CreateWindow(
+        "shape",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        1024,
+        768,
+        SDL_WINDOW_OPENGL
+    );
+    
+    if (window == NULL) {
+        return 1;
+    }
+    
+    SDL_Delay(1000);
+    
+    SDL_DestroyWindow(window);
+     
+    SDL_Quit();
+    return 0;
 }
